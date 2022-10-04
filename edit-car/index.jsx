4485ddad6fe3c4c22f7e12e92633f7545f965d6b
yasintasyara <89920769+yasintasyara@users.noreply.github.com>
2022-10-04 16:51:23 +0700

    add edit-car-page

---
+++ edit-car/index.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import Header from './header'
+import Editcarform from './editCarForm'
+import "./index.css"
+
+const Index = () => {
+    return (
+        <Fragment>
+            <Header />
+            <Editcarform />
+        </Fragment>
+    )
+}
+
+export default Index
